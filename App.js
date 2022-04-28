ff37b93392aba14ab40fecab23b5555c5eae3a6c
“T3sultan” <incognito.soul011@gmail.com>
2022-04-28 14:18:51 +0600

    indicator

--- App.js
+++ App.js
@@ -7,11 +7,15 @@ import {
   Dimensions,
   StyleSheet,
   Text,
+  Animated,
 } from "react-native";
 const { width, height } = Dimensions.get("screen");
 
 const ITEM_WIDTH = width;
 const ITEM_HEIGHT = height * 0.75;
+const DOT_SIZE = 7;
+const DOT_SPACING = 8;
+const DOT_INDICATOR_SIZE = DOT_SIZE + DOT_SPACING;
 
 const images = [
   "https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_1_1_1.jpg?ts=1606727905128",
@@ -28,44 +32,86 @@ const product = {
     'Height x Length x Width: 14 x 21.5 x 4.5 cm. / 5.5 x 8.4 x 1.7"',
   ],
 };
-const renderNewPlants = (item, index) => {
-  return (
-    <View style={{ width, height }}>
-      <Image
-        source={{ uri: item }}
-        style={{
-          height: ITEM_HEIGHT,
-          width: ITEM_WIDTH,
-          resizeMode: "cover",
-        }}
-      />
-    </View>
-  );
-};
 
 export default function App() {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <FlatList
-        data={images}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item, index }) => renderNewPlants(item, index)}
-      />
+      <View style={{ height: ITEM_HEIGHT, overflow: "hidden" }}>
+        <Animated.FlatList
+          data={images}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(_, index) => index.toString()}
+          snapToInterval={ITEM_HEIGHT}
+          decelerationRate="fast"
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: true }
+          )}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <Image source={{ uri: item }} style={styles.image} />
+              </View>
+            );
+          }}
+        />
+        <View style={styles.pagination}>
+          {images.map((_, index) => {
+            return <View key={index} style={[styles.dot]} />;
+          })}
+          <Animated.View
+            style={[
+              styles.dotIndicator,
+              {
+                transform: [
+                  {
+                    translateY: Animated.divide(
+                      scrollY,
+                      ITEM_HEIGHT
+                    ).interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, DOT_INDICATOR_SIZE],
+                    }),
+                  },
+                ],
+              },
+            ]}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
   image: {
     width: ITEM_WIDTH,
     height: ITEM_HEIGHT,
     resizeMode: "cover",
+  },
+  pagination: {
+    position: "absolute",
+    top: ITEM_HEIGHT / 2,
+    left: 20,
+  },
+  dot: {
+    width: DOT_SIZE,
+    height: DOT_SIZE,
+    borderRadius: DOT_SIZE,
+    backgroundColor: "#000",
+    marginBottom: DOT_SPACING,
+  },
+  dotIndicator: {
+    width: DOT_INDICATOR_SIZE,
+    height: DOT_INDICATOR_SIZE,
+    borderRadius: DOT_INDICATOR_SIZE,
+    borderWidth: 1,
+    borderColor: "#333",
+    position: "absolute",
+    top: -DOT_SIZE / 2,
+    left: -DOT_SIZE / 2,
   },
 });
